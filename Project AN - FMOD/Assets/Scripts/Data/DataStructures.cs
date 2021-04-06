ae2526dfc41e55386be7ea74cdb66b5efb530e92
Alfie Luo <yl5209@rit.edu>
2021-04-06 10:30:17 -0400

    Added game project. Added sample sounds. Added FMOD project.

---
+++ Project AN - FMOD/Assets/Scripts/Data/DataStructures.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pair<T, U>
+{
+    public Pair()
+    {
+    }
+
+    public Pair(T first, U second)
+    {
+        this.First = first;
+        this.Second = second;
+    }
+
+    public T First { get; set; }
+    public U Second { get; set; }
+};
+
+
+public class Coord<T, U>
+{
+    public Coord()
+    {
+    }
+
+    public Coord(T x, U y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public T x { get; set; }
+    public U y { get; set; }
+};
